1bef737d9c74c8830669aeecd6b1e0ddd5c523eb
kristjan.konsap <kristjan.konsap@khk.ee>
2019-02-14 11:34:47 +0200

    uus_menu loodud menu lehe contentTmpl

--- uus_menu/index.php
+++ uus_menu/index.php
@@ -1,28 +1,18 @@
 <?php
-//require_once 'lib/fnk.php';
-//require_once 'menus/menu.php';
-//// lehe sisu
-//// päis - header ja muud vajalikud osad
-//pais();
-//// menüü elemendid - menu funktsioon kasutab menu_element funktsiooni sisu loomiseks
-//menu(array(
-//    'praed' => $praed,
-//    'supid' => $supid,
-//    'magus' => $magusad,
-//    'joogid' => $joogid
-//));
-//// jalus - kõik JS asjad
-//jalus();
-
 require_once 'conf.php';
 //echo '<pre>';
 //print_r($sess);
 //echo '<pre>';
 
+//vajalikud vaate objektid
 $mainTmpl = new Template('main');
+$contentTmpl = new Template('content');
 
 $mainTmpl ->set('title', 'Menu App');
-$mainTmpl ->set('content', 'Menu App Content');
+//$contentTmpl->set('')
+
+
+$mainTmpl ->set('content', $contentTmpl->parse());
 
 echo $mainTmpl->parse();
 
