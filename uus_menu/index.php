bee10b06f2b36a7601f061dbf2a05af38ecda9ea
kristjan.konsap <kristjan.konsap@khk.ee>
2019-02-14 11:07:19 +0200

    uus_menu index.php session

--- uus_menu/index.php
+++ uus_menu/index.php
@@ -1,16 +1,21 @@
 <?php
-require_once 'lib/fnk.php';
-require_once 'menus/menu.php';
-// lehe sisu
-// päis - header ja muud vajalikud osad
-pais();
-// menüü elemendid - menu funktsioon kasutab menu_element funktsiooni sisu loomiseks
-menu(array(
-    'praed' => $praed,
-    'supid' => $supid,
-    'magus' => $magusad,
-    'joogid' => $joogid
-));
-// jalus - kõik JS asjad
-jalus();
+//require_once 'lib/fnk.php';
+//require_once 'menus/menu.php';
+//// lehe sisu
+//// päis - header ja muud vajalikud osad
+//pais();
+//// menüü elemendid - menu funktsioon kasutab menu_element funktsiooni sisu loomiseks
+//menu(array(
+//    'praed' => $praed,
+//    'supid' => $supid,
+//    'magus' => $magusad,
+//    'joogid' => $joogid
+//));
+//// jalus - kõik JS asjad
+//jalus();
+
+require_once 'conf.php';
+echo '<pre>';
+print_r($sess);
+echo '<pre>';
 ?>
