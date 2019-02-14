62b190973b2ba800546226f12481a9c16d0472a0
kristjan.konsap <kristjan.konsap@khk.ee>
2019-02-14 11:18:56 +0200

    uus_menu loodud testimise peatemplate

--- uus_menu/index.php
+++ uus_menu/index.php
@@ -15,7 +15,18 @@
 //jalus();
 
 require_once 'conf.php';
-echo '<pre>';
-print_r($sess);
-echo '<pre>';
+//echo '<pre>';
+//print_r($sess);
+//echo '<pre>';
+
+$mainTmpl = new Template('main');
+
+$mainTmpl ->set('title', 'Menu App');
+$mainTmpl ->set('content', 'Menu App Content');
+
+echo $mainTmpl->parse();
+
+//echo '<pre>';
+//print_r($mainTmpl);
+//echo '<pre>';
 ?>
