124d03145c404f1fc21546682e083e78b0855546
kristjan.konsap <kristjan.konsap@khk.ee>
2019-02-21 10:28:02 +0200

    uus_menu index.php-st -> menu.php-sse

--- uus_menu/index.php
+++ uus_menu/index.php
@@ -3,39 +3,11 @@ require_once 'conf.php';
 // loome vajalikud vaade objektid
 $mainTmpl = new Template('main');
 $contentTmpl = new Template('content');
-$categories = array(
-    array(
-        'name' => 'praed',
-        'icon' => 'fa-utensils',
-        'data' => array(
-            array(
-                'name' => 'Sealihapada ploomide ja aprikoosiga',
-                'description' => 'sealihapada, lisand, salat, leib',
-                'price' => 2.65,
-                'discount' => 2.25
-            ),
-            array(
-                'name' => 'Praetud kanakints',
-                'description' => 'praetud kana, lisand, salat, leib',
-                'price' => 2.50,
-                'discount' => 2.13
-            )
-        )
-    ),
-    array(
-        'name' => 'supid',
-        'icon' => 'fa-utensil-spoon',
-        'data' => array(
-            array(
-                'name' => 'Rassolnik',
-                'description' => 'supp, hapukoor, leib',
-                'price' => 1.10,
-                'discount' => 0.94
-            )
-        )
-    )
-);
+
 $mainTmpl->set('title', 'Menu App');
+
+require_once 'menus/menu.php';
+
 foreach ($categories as $category){
     $cardTmpl = new Template('card');
     $cardHeaderTmpl = new Template('header');
