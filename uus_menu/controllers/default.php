f3a1668d1c2473d70a8f7708949d8bdf64987831
kristjan.konsap <kristjan.konsap@khk.ee>
2019-02-21 10:37:59 +0200

    uus_menu index.php-st -> controllers.php-sse

---
+++ uus_menu/controllers/default.php
@@ -0,0 +1,29 @@
+<?php
+// loome vajalikud vaade objektid
+$mainTmpl = new Template('main');
+$contentTmpl = new Template('content');
+
+$mainTmpl->set('title', 'Menu App');
+
+require_once 'menus/menu.php';
+
+foreach ($categories as $category){
+    $cardTmpl = new Template('card');
+    $cardHeaderTmpl = new Template('header');
+    $cardDataTmpl = new Template('data');
+    $cardHeaderTmpl->set('category', $category['name']);
+    $cardHeaderTmpl->set('icon', $category['icon']);
+    $cardTmpl->set('card_header', $cardHeaderTmpl->parse());
+    $cardDataTmpl->set('category', $category['name']);
+    $listTmpl = new Template('list');
+    foreach ($category['data'] as $dish){
+        foreach ($dish as $name=>$value){
+            $listTmpl->set($name, $value);
+        }
+        $cardDataTmpl->add('dish_list', $listTmpl->parse());
+    }
+    $cardTmpl->set('card_data', $cardDataTmpl->parse());
+    $contentTmpl->add('cards', $cardTmpl->parse());
+}
+$mainTmpl->set('content', $contentTmpl->parse());
+echo $mainTmpl->parse();
